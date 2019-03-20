907dc8c96034c011525ce7d85a8329a37f0132be
StanislavFedyna <stanislavfedyna@gmail.com>
2019-03-20 13:30:07 +0200

    included check-expression

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -17,16 +17,20 @@ class Recipe extends React.Component{
     });
   }
   render(){
-    const {image_url, title, publisher} = this.state.activeRecipe;
+    const {image_url, title, publisher, publisher_url} = this.state.activeRecipe;
 
     return(
       <div class="container">
+      {
+        this.state.activeRecipe.length !== 0 && 
         <div class="active-recipe">
-          <img className="active-recipe__img" src={ image_url } alt={ title }/>
-          <h3 className="active-recipe__title">{ title }</h3>
-          <h4 className="active-recipe__publisher">Publisher: <span>{ publisher }</span></h4>
-          
-        </div>
+        <img className="active-recipe__img" src={ image_url } alt={ title }/>
+        <h3 className="active-recipe__title">{ title }</h3>
+        <h4 className="active-recipe__publisher">Publisher: <span>{ publisher }</span></h4>
+        <p className="active-recipe__website">Website: <span><a href={publisher_url}>{publisher_url}</a></span></p>
+        <button className="active-recipe__button">Go Home</button>
+      </div>
+      }
       </div>
     )
   }
