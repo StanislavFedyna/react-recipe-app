9b63ce783a589f58a4d885a004d93f20810edbb9
StanislavFedyna <stanislavfedyna@gmail.com>
2019-03-20 13:59:52 +0200

    fixing problem with local storage

--- src/App.js
+++ src/App.js
@@ -15,24 +15,22 @@ class App extends Component {
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
-
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    
     const data = await api_call.json();
-    this.setState({
-      recipes: data.recipes
-    })
-    console.log(this.state.recipes)
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   }
-  componentDidMount = () =>{
-    const json = localStorage.getItem("recipes");
-    const recipes = JSON.parse(json);
-  }
+  // componentDidMount = () => {
+  //   const json = localStorage.getItem("recipes");
+  //   const recipes = JSON.parse(json);
+  //   this.setState({ recipes });
+  // }
+  // componentDidUpdate = () => {
+  //   const recipes = JSON.stringify(this.state.recipes);
+  //   localStorage.setItem("recipes", recipes);
+  // }
 
-  componentDidUpdate = () =>{
-     const recipes = JSON.stringify(this.state.recipes);
-     localStorage.setItem("recipes", recipes);
-     this.setState({recipes: recipes})
-  }
 
   render() {
     return (
